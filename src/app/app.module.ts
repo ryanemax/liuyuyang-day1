0bee49c478725cbcbf82725dd0125908fc3ff6a3
Wang Jiayin <315578188@qq.com>
2017-08-27 17:38:48 +0800

    resolve conflict

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EmployeeEditPageComponent } from './employee-edit-page/employee-edit-page.component';
-
 import { NoopAnimationsModule } from '@angular/platform-browser/animations'
 
 
