36be9de3842619b98bfd0626c5c70f07afb47d88
Jiang <minghua.jiang@accenture.com>
2017-08-27 19:18:04 +0800

    update:cookbook

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -116,6 +116,12 @@ const SSRroutes: Routes = [
     loadChildren: '../pages/game/game.module#GameModule',
     data:{title:"游戏排行榜" }
   },
+
+  {
+    path: 'cookbook', 
+    loadChildren: '../pages/cookbook/cookbook.module#CookbookModule',
+    data:{title:"食谱烹饪" }
+  },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
 
