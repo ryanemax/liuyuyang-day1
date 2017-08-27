6c7c6277b42bc63814f79424c5609849b1b97fd0
PengL <1733531186@qq.com>
2017-08-27 15:18:07 +0800

    add asset page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,7 @@ const SSRroutes: Routes = [
   { path: 'about', loadChildren: '../pages/about/about.module#AboutModule' },
   { path: 'contact', loadChildren: '../pages/contact/contact.module#ContactModule' },
   { path: 'movie', loadChildren: '../pages/movie/movie.module#MovieModule' },
+  { path: 'asset', loadChildren: '../pages/asset/asset.module#AssetModule' ,data:{title:"我的衣帽间"}},
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
 
