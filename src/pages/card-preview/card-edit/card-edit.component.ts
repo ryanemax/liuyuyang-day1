d9d46d6d830f52560bbfc7be358e95c66512ce8a
accenture <ubq@qq.com>
2017-08-27 17:59:25 +0800

    update:added edit page

---
+++ src/pages/card-preview/card-edit/card-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-edit',
+  templateUrl: './card-edit.component.html',
+  styleUrls: ['./card-edit.component.scss']
+})
+export class CardEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
