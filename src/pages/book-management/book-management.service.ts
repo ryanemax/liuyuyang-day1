27015faaa254717a6d5f3b8f40e300107e471db7
lunjiang <lun.a.jiang@outlook.com>
2017-08-28 16:50:12 +0800

    feat add book-management DB

--- src/pages/book-management/book-management.service.ts
+++ src/pages/book-management/book-management.service.ts
@@ -1,33 +1,47 @@
 import { Injectable } from '@angular/core';
-import { Observable } from "rxjs/Observable"
+import { Observable } from "rxjs/Observable";
+import { Http, Headers } from '@angular/http';
+
 import 'rxjs/add/observable/merge';
 import 'rxjs/add/observable/of';
 
 @Injectable()
 export class BookManagementService {
+    // HTTP Params
+    authHeaders:Headers = new Headers()
+    host = "http://47.92.145.25:2337/parse"
+    className = "MyBook"
+
   books:Array<JianglunBook>;
-  constructor() { 
-    this.getBooks()
+
+  constructor(private http:Http) { 
+    this.authHeaders.append("X-Parse-Application-Id","dev")
+    this.authHeaders.append("X-Parse-Master-Key","angulardev")
+    this.authHeaders.append("Content-Type","application/json")
   }
 
-  getBookByBookIndex(index):Observable<JianglunBook>{
-    let book = this.books.find(item=>item.index == index)
-    return Observable.of(book)
+  getBookById(id):Observable<Book>{
+
+    let url = this.host+"/classes/" + this.className + "/" + id
+    let options = {
+      headers:this.authHeaders
+    }
+
+    return this.http
+    .get(url,options)
+    .map(data=>data.json())
   }
 
-  getBooks(){
-     this.books = [
-      {index:1, bookName:"深入理解ES6", author:"Nicholas，C.，Zakas", price:78.20, 
-      summary:"参透ECMAScript标准不啻前端通行证 随一代宗师参详JavaScript精进之路", random:Math.random()},
-      {index:2, bookName:"Node.js区块链开发", author:"朱志文", price:45.40, 
-      summary:"这是一本基于一个实际运行的区块链产品，从代码层面深度完整讲述区块链开发的书籍；这是一本开源产品的实践书籍，通过工程思维，讲解工程实践，真正体现编程技术的工具价值。", random:Math.random()},
-      {index:3, bookName:"架构解密：从分布式到微服务", author:"Leader-us", price:66.20, 
-      summary:"历经三年终出版！从你想了解的源码、理论、经验、视野等维度，对从分布式到微服务，进行架构解密！", random:Math.random()},
-      {index:4, bookName:"Hadoop构建数据仓库实践", author:"王雪迎", price:70.30, 
-      summary:"通过构建数据仓库，深入学习Hadoop, 轻松掌握大数据技术", random:Math.random()},
-      {index:5, bookName:"MongoDB实战（第二版）", author:"Kyle，Banker，Peter，Bakkum，Shaun ...", price:59.10, 
-      summary:"MongoDB官方团队编写，架构师必备图书。", random:Math.random()}
-    ]
+  getBooks():Observable<Array<Book>>{
+    
+    let url = this.host+"/classes/" + this.className
+    let options = {
+      headers:this.authHeaders
+    }
+
+    return this.http
+    .get(url,options)
+    .map(data=>data.json().results)
   }
 
   addBook(book){
