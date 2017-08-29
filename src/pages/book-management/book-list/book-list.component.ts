6c6516768199a6a4ad2bb9e5f0d78191f5b0121f
lunjiang <lun.a.jiang@outlook.com>
2017-08-29 09:57:05 +0800

    update book-management

--- src/pages/book-management/book-list/book-list.component.ts
+++ src/pages/book-management/book-list/book-list.component.ts
@@ -7,25 +7,63 @@ import { BookManagementService } from "../book-management.service"
   styleUrls: ['./book-list.component.scss']
 })
 export class BookListComponent implements OnInit {
+  index : number = 1;
   books : Array<JianglunBook>;
   @Input() book:any;
   constructor(private bookServ:BookManagementService) {
-    this.books = this.bookServ.books;
+    let index = this.index;
+    this.bookServ.getBooks().subscribe(data=>{
+      this.books = data
+      this.books.forEach(book => book.index = index++);
+    })
+    
   }
 
   delete(book){
-    this.bookServ.deleteByName(book.bookName)
+    this.bookServ.deleteById(book.objectId).subscribe(data=>{
+        location.href = "/book-management"
+    })
   }
 
   ascPrice(){
-    this.bookServ.ascPrice()
-  }
+    // 价格正序排列
+    // 数组操作API，https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+    let index = this.index;
+    this.books.sort((a,b)=>{
+      if(a.price>b.price){
+        return 1
+      }else{
+        return -1
+      }
+    }).forEach(book => book.index = index++);
+}
+
   descPrice(){
-    this.bookServ.descPrice()
+    // 价格逆序排列    
+    let index = this.index;
+    this.books.sort((a,b)=>{
+      if(a.price<b.price){
+        return 1
+      }else{
+        return -1
+      }
+    }).forEach(book => book.index = index++);
   }
+  
   random(){
-    this.bookServ.random()
+    // 随机排列
+    // 常用数学计算API，https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+    let index = this.index;
+    this.books.forEach((item) => {item.random=Math.random()});
+    this.books.sort((a,b)=>{
+      if(a.random<b.random){
+        return 1
+      }else{
+        return -1
+      }
+    }).forEach(book => book.index = index++);
   }
+
   ngOnInit() {
   }
 
--- src/pages/book-management/book-management.service.ts
+++ src/pages/book-management/book-management.service.ts
@@ -4,6 +4,7 @@ import { Http, Headers } from '@angular/http';
 
 import 'rxjs/add/observable/merge';
 import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class BookManagementService {
@@ -12,15 +13,13 @@ export class BookManagementService {
     host = "http://47.92.145.25:2337/parse"
     className = "MyBook"
 
-  books:Array<JianglunBook>;
-
   constructor(private http:Http) { 
     this.authHeaders.append("X-Parse-Application-Id","dev")
     this.authHeaders.append("X-Parse-Master-Key","angulardev")
     this.authHeaders.append("Content-Type","application/json")
   }
 
-  getBookById(id):Observable<Book>{
+  getBookById(id):Observable<JianglunBook>{
 
     let url = this.host+"/classes/" + this.className + "/" + id
     let options = {
@@ -32,65 +31,50 @@ export class BookManagementService {
     .map(data=>data.json())
   }
 
-  getBooks():Observable<Array<Book>>{
+  getBooks():Observable<Array<JianglunBook>>{
     
     let url = this.host+"/classes/" + this.className
     let options = {
       headers:this.authHeaders
     }
 
+    return this.http.get(url,options).map(data=>data.json().results)
+  }
+
+  saveBook(book){
+    // this.http.post()
+    let url = this.host+"/classes/" + this.className
+    let options = {
+      headers:this.authHeaders
+    }
+    
+    if(book.objectId){
+      let id = book.objectId
+      delete book.createdAt
+      delete book.updatedAt
+      delete book.objectId
+      delete book.ACL
+
+      return this.http
+      .put(url+"/"+id,book,options)
+      .map(data=>data.json())
+    }else{
+      return this.http
+      .post(url,book,options)
+      .map(data=>data.json())
+    }
+  }
+
+  deleteById(id){
+    let url = this.host+"/classes/" + this.className + "/" + id
+    let options = {
+      headers:this.authHeaders
+    }
+
     return this.http
-    .get(url,options)
-    .map(data=>data.json().results)
-  }
-
-  addBook(book){
-    book.index = this.books.length + 1;
-    this.books.push(book)
+    .delete(url,options)
+    .map(data=>data.json())
   }
   
-  deleteByName(bookName){
-    this.books.forEach((item,index,arr)=>{
-      if(item.bookName == bookName){
-        arr.splice(index,1)
-      }
-    })
-  }
-  
-  ascPrice(){
-    // 价格正序排列
-    // 数组操作API，https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
-    this.books.sort((a,b)=>{
-      if(a.price>b.price){
-        return 1
-      }else{
-        return -1
-      }
-    })
-}
-
-  descPrice(){
-    // 价格逆序排列    
-    this.books.sort((a,b)=>{
-      if(a.price<b.price){
-        return 1
-      }else{
-        return -1
-      }
-    })
-  }
-  
-  random(){
-    // 随机排列
-    // 常用数学计算API，https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
-    this.books.forEach((item) => {item.random=Math.random()});
-    this.books.sort((a,b)=>{
-      if(a.random<b.random){
-        return 1
-      }else{
-        return -1
-      }
-    })
-  }
 
 }
