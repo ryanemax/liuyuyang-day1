c0d0b928f51ee6208305f4fae74e4ad1270852cc
Jiang <minghua.jiang@accenture.com>
2017-08-30 14:44:04 +0800

    commit cookbook

--- src/pages/cookbook/cookbook.module.ts
+++ src/pages/cookbook/cookbook.module.ts
@@ -4,13 +4,12 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { CookListPageComponent } from './cook-list-page/cook-list-page.component';
 import { CookbookEditComponent } from './cookbook-edit/cookbook-edit.component';
+import { CookbookAnalysisComponent } from './cookbook-analysis/cookbook-analysis.component';
 
 // 引入第三方共享模块
 import { SharedModule } from "../../shared/shared.module";
 import { MaterialModule } from "@angular/material";
 import { SharedEditModule } from "../../shared-edit/shared-edit.module";
-
-
 
 import { CookbookService } from "./cookbook.service"
 
@@ -20,12 +19,13 @@ import { CookbookService } from "./cookbook.service"
     SharedModule,MaterialModule,
     RouterModule.forChild([
       { path: '', component: CookListPageComponent, pathMatch: 'full' }, // 页面/：参数
-      { path: 'edit/:id', component: CookbookEditComponent, pathMatch: 'full' }
+      { path: 'edit/:id', component: CookbookEditComponent, pathMatch: 'full' },
+      { path: 'analysis/:id', component: CookbookEditComponent, pathMatch: 'full' }
      
     ])
   ],
   providers:[CookbookService],
-  declarations: [CookListPageComponent,CookbookEditComponent]
+  declarations: [CookListPageComponent,CookbookEditComponent,CookbookAnalysisComponent]
  
 })
 export class CookbookModule { }
