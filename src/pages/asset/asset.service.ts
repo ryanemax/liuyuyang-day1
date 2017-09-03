ca5a8f4df53f4d0a36179ff920c9da33f673c8eb
PengL <1733531186@qq.com>
2017-09-03 17:37:29 +0800

    asset : data initialize

--- src/pages/asset/asset.service.ts
+++ src/pages/asset/asset.service.ts
@@ -25,7 +25,7 @@ export class AssetService {
   assets: Array<Asset>;
 
   // 修改时的编辑对象
-  editObject:Asset;
+  editObject: Asset;
 
   constructor(private http: Http) {
     this.authHeaders.append("X-Parse-Application-Id", "dev")
@@ -46,7 +46,7 @@ export class AssetService {
   //       no: 1, name: "ONLY黑色连帽卫衣",
   //       classification: "上衣",
   //       brand: "ONLY",
-  //       price: 244, addTime: new Date("2016-03-03"), img: "../../../assets/img/asset/1.jpg"
+  //       price: 244, addTime: new Date("2017-04-02"), img: "../../../assets/img/asset/1.jpg"
   //     }, {
   //       no: 2, name: "ONLY荷叶边连衣裙",
   //       classification: "裙装",
@@ -56,22 +56,46 @@ export class AssetService {
   //       no: 3, name: "ONLY修身小脚牛仔裤",
   //       classification: "裤装",
   //       brand: "ONLY",
-  //       price: 199, addTime: new Date("2016-10-05"), img: "../../../assets/img/asset/3.jpg"
+  //       price: 199, addTime: new Date("2016-05-05"), img: "../../../assets/img/asset/3.jpg"
   //     }, {
   //       no: 4, name: "平底绣花布鞋",
   //       classification: "鞋靴",
   //       brand: "老北京布鞋",
-  //       price: 29, addTime: new Date("2016-10-05"), img: "../../../assets/img/asset/4.jpg"
+  //       price: 29, addTime: new Date("2013-10-05"), img: "../../../assets/img/asset/4.jpg"
   //     }, {
   //       no: 5, name: "恒源祥夏用披肩",
   //       classification: "围巾",
   //       brand: "恒源祥",
-  //       price: 138, addTime: new Date("2017-07-03"), img: "../../../assets/img/asset/5.jpg"
+  //       price: 138, addTime: new Date("2017-08-01"), img: "../../../assets/img/asset/5.jpg"
   //     }, {
   //       no: 6, name: "ZARA几何图形印花晚宴包",
   //       classification: "包包",
   //       brand: "ZARA",
-  //       price: 399, addTime: new Date("2017-08-10"), img: "../../../assets/img/asset/6.jpg"
+  //       price: 399, addTime: new Date("2013-08-10"), img: "../../../assets/img/asset/6.jpg"
+  //     }
+  // }, {
+  //       no: 7, name: "花花公子内增高女鞋",
+  //       classification: "鞋靴",
+  //       brand: "花花公子",
+  //       price: 199, addTime: new Date("2016-11-05"), img: "../../../assets/img/asset/7.jpg"
+  //     }
+  // }, {
+  //       no: 8, name: "花花公子厚底小白鞋",
+  //       classification: "鞋靴",
+  //       brand: "花花公子",
+  //       price: 139, addTime: new Date("2015-03-20"), img: "../../../assets/img/asset/8.jpg"
+  //     }
+  // }, {
+  //       no: 9, name: "ZARA铆钉饰平底粗跟女鞋",
+  //       classification: "鞋靴",
+  //       brand: "ZARA",
+  //       price: 188, addTime: new Date("2015-09-07"), img: "../../../assets/img/asset/9.jpg"
+  //     }
+  // }, {
+  //       no: 10, name: "韩都衣舍长款毛领羽绒服",
+  //       classification: "上衣",
+  //       brand: "韩都衣舍",
+  //       price: 318, addTime: new Date("2014-10-08"), img: "../../../assets/img/asset/10.jpg"
   //     }
   //   ]
   // }
@@ -149,7 +173,7 @@ export class AssetService {
   connect() {
     return this.dataChange
   }
-  
+
   refresh() {
     this.getAssets().subscribe(data => {
       this.dataChange.next(data);
@@ -163,7 +187,7 @@ export class AssetService {
       this.assets = data
       this.dataChange.next(data);
     })
-    
+
   }
 
 }
