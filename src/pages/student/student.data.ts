52a0199c7928ee354c4504b7067a416b13b1f6cc
RyaneMax <ryanemax@gmail.com>
2017-09-07 10:34:02 +0800

    update: student.data.ts day 1 grades

--- src/pages/student/student.data.ts
+++ src/pages/student/student.data.ts
@@ -35,10 +35,190 @@ function makeStudents(){
      'sex':'M',
      'github':'zhayong',
      'desc':'',
-     'exam1': 0, // 10 10 5 5 20 5 zhayong/zhayong-exam.git
+     'exam1': 70, // zhayong/zhayong-exam.git
      'exam2': 0, // 5 10 20 15 15 10
      'exam3': 0, // 10 5 10 10 15
      'exam4': 0 // 10 30 20 20 10
+    },
+    {
+     'name':'zhangyu',
+     'sex':'M',
+     'github':'zhangyu',
+     'desc':'',
+     'exam1': 60, // fireyu1980/zhangyu-exam
+     'exam2': 0, //
+     'exam3': 0, //
+     'exam4': 0 //
+    },
+    {
+     'name':'Haolong.guo',
+     'sex':'M',
+     'github':'longerZ',
+     'desc':'',
+     'exam1': 70, // longerZ/test_day1
+     'exam2': 0, //
+     'exam3': 0, //
+     'exam4': 0 //
+    },
+    {
+     'name':'geneqiang',
+     'sex':'M',
+     'github':'geneqiang',
+     'desc':'',
+     'exam1': 75, // geneqiang/cq-day1
+     'exam2': 0, //
+     'exam3': 0, //
+     'exam4': 0 //
+    },
+    {
+     'name':'石文强',
+     'sex':'M',
+     'github':'shiwenqiang456',
+     'desc':'',
+     'exam1': 60, //
+     'exam2': 0, //
+     'exam3': 0, //
+     'exam4': 0 //
+    },
+    {
+     'name':'qianjinjiayin',
+     'sex':'M',
+     'github':'qianjinjiayin',
+     'desc':'',
+     'exam1': 40, //
+     'exam2': 0, //
+     'exam3': 0, //
+     'exam4': 0 //
+    },
+    {
+     'name':'Zhang Shuai',
+     'sex':'M',
+     'github':'zs2love',
+     'desc':'',
+     'exam1': 90, //
+     'exam2': 0, //
+     'exam3': 0, //
+     'exam4': 0 //
+    },
+    {
+     'name':'hansheng',
+     'sex':'M',
+     'github':'xiaohanfeidao',
+     'desc':'',
+     'exam1': 85, //
+     'exam2': 0, //
+     'exam3': 0, //
+     'exam4': 0 //
+    },
+    {
+     'name':'姜鸣华',
+     'sex':'F',
+     'github':'JenniferJiangminghua',
+     'desc':'',
+     'exam1': 70, //
+     'exam2': 0, //
+     'exam3': 0, //
+     'exam4': 0 //
+    },
+    {
+     'name':'彭蕾',
+     'sex':'F',
+     'github':'PengLei1992',
+     'desc':'',
+     'exam1': 75, //
+     'exam2': 0, //
+     'exam3': 0, //
+     'exam4': 0 //
+    },
+    {
+     'name':'Haoling0520',
+     'sex':'M',
+     'github':'Haoling0520',
+     'desc':'',
+     'exam1': 60, //
+     'exam2': 0, //
+     'exam3': 0, //
+     'exam4': 0 //
+    },
+    {
+     'name':'yangkaizhe',
+     'sex':'M',
+     'github':'buffoonMen',
+     'desc':'',
+     'exam1': 40, //
+     'exam2': 0, //
+     'exam3': 0, //
+     'exam4': 0 //
+    },
+    {
+     'name':'WuXiaoxu',
+     'sex':'M',
+     'github':'xiaoxu555',
+     'desc':'',
+     'exam1': 60, //
+     'exam2': 0, //
+     'exam3': 0, //
+     'exam4': 0 //
+    },
+    {
+     'name':'刘超',
+     'sex':'F',
+     'github':'lctsp',
+     'desc':'',
+     'exam1': 60, //
+     'exam2': 0, //
+     'exam3': 0, //
+     'exam4': 0 //
+    },
+    {
+     'name':'姜仑',
+     'sex':'M',
+     'github':'lunjiang',
+     'desc':'',
+     'exam1': 70, //
+     'exam2': 0, //
+     'exam3': 0, //
+     'exam4': 0 //
+    },
+    {
+     'name':'姜胜',
+     'sex':'M',
+     'github':'ubqubq',
+     'desc':'',
+     'exam1': 70, //
+     'exam2': 0, //
+     'exam3': 0, //
+     'exam4': 0 //
+    },
+    {
+     'name':'bluesy17',
+     'sex':'M',
+     'github':'bluesy17',
+     'desc':'',
+     'exam1': 65, //
+     'exam2': 0, //
+     'exam3': 0, //
+     'exam4': 0 //
+    },
+    {
+     'name':'betsyxqq',
+     'sex':'M',
+     'github':'betsyxqq',
+     'desc':'',
+     'exam1': 75, //
+     'exam2': 0, //
+     'exam3': 0, //
+     'exam4': 0 //
+    },
+    {
+     'name':'姜胜',
+     'sex':'M',
+     'github':'ubqubq',
+     'desc':'',
+     'exam1': 70, //
+     'exam2': 0, //
+     'exam3': 0, //
+     'exam4': 0 //
     }
   ];
 }
